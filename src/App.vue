187d8d10f5a4eb2c10c5b830d6e7508df77aea69
TaySabrina <taysabrina.prog@gmail.com>
2021-03-13 12:12:21 -0300

    pagina de inicio e top bar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,59 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div>
+    <v-app-bar app color="indigo darken-4" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title><v-icon>mdi-home</v-icon>Coleta Inteligente</v-toolbar-title>
+      
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cadastro Pontos de Coleta</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Lista de Pontos de Coleta</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Detalhes sobre Pontos de Coleta</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cadastro de Usuários</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <router-view></router-view>
+      <router-view/>
     </v-main>
-  </v-app>
+  </div>
+  
 </template>
 
 <script>
@@ -49,7 +62,8 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h2 class="text-h5 text-center mb-3 mt-5">Coleta Seletiva</h2>
+     <v-img
+      class="inicio-imag mb-5 mt-10"
+      src="../assets/coleta-seletiva.png">
+    </v-img>
+    <p class="pt-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere iure atque dicta, aut iste eveniet labore optio illum saepe voluptatum facilis harum iusto cum quisquam beatae dolorum impedit deserunt eaque.</p>
+   
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Inicio'
+}
+</script>
