e8f6fd0e5de8c9836bed7e085cd2ecc674e4eae6
Roberto Hamano <robertokenzohamano@gmail.com>
2021-03-13 12:07:06 -0300

    Fix: uncommenting import

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Home from '../views/Home.vue'
 import PointsList from '../views/PointsList.vue'
 
 Vue.use(VueRouter)
