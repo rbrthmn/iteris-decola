4fa2a61dba6003b80c1f720e22b3a1623af83550
TaySabrina <taysabrina.prog@gmail.com>
2021-03-13 13:20:08 -0300

    mudando a cor da pagina inicial

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,12 +1,11 @@
 <template>
   <v-container class="inicio-pagina">
-    <h2 class="text-h5 text-center mb-3 mt-5">Coleta Seletiva</h2>
-     <v-img
+    <h2 class="text-h5 text-center mb-3 mt-5">Coletamos o seu lixo</h2>
+    <p class="texto-pagina">A Eco-Coleta é uma empresa especializada em pontos de coleta seletiva de resíduos. Atendemos pessoas físicas, jurídicas e realizamos novos cadastros.</p>
+    <v-img
       class="inicio-imag"
       src="../assets/coleta-seletiva.png">
     </v-img>
-    <p class="texto-pagina">A Eco-Coleta é uma empresa especializada em pontos de coleta seletiva de resíduos. Atendemos pessoas físicas e jurídicas e realizamos novos cadastros.</p>
-   
   </v-container>
 </template>
 
@@ -22,14 +21,18 @@ export default {
     flex-direction: column;
     align-items: center;
     padding-top: 10px;
+    background-color: cornsilk;
     justify-content: space-around;
   }
   .texto-pagina{
-    padding-top: 15px;
+    padding-top: 20px;
     text-align: center;
-    font-weight: bold;
+    
+    font-size: large;
   }
   .v-image{
     margin-top: 20px;
+    background-color:cornsilk;
   }
+
 </style>
